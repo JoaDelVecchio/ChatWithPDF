9068321bad9d8daae38375f415fdaa5ae8de0cba
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2025-02-18 13:45:44 -0300

    Change 'n' for and to avoid deployment errors

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -42,7 +42,7 @@ const FileUploader = () => {
         ) : (
           <>
             <CircleArrowDown className="h-20 w-20 animate-bounce" />
-            <p>Drag 'n' drop some files here, or click to select files</p>
+            <p>Drag and drop some files here, or click to select files</p>
           </>
         )}
       </div>
