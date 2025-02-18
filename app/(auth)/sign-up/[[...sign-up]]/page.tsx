21e81938b17e123e015fd98f836d554bf8179213
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2025-02-17 21:16:38 -0300

    Add sign in signout pages

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <SignIn signUpUrl="/sign-up" />
+      <SignUp signInUrl="/sign-in" />
     </div>
   );
 }
